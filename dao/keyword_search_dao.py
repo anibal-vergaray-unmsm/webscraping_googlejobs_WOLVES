382523b586c9b51a627a70201d5c7086293b8063
DavidVC <51352843+DavidVegaC@users.noreply.github.com>
2021-02-06 16:56:01 -0500

    Update keyword_search_dao.py

--- dao/keyword_search_dao.py
+++ dao/keyword_search_dao.py
@@ -9,7 +9,7 @@ class KeywordSearchDao():
     def select_keyword_search(self):
         # defino las sentencia sql
 
-        sql_select = "SELECT id_keyword, descripcion FROM public.keyword_search where id_keyword between 57 and 57"
+        sql_select = "SELECT id_keyword, descripcion FROM public.keyword_search"
 
 
         return self.__repository.select_keyword_search(sql_select)
