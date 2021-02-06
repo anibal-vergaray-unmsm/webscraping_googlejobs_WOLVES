0ef6deb6635c7093d376a92c2adcb665766ff0ad
DavidVC <51352843+DavidVegaC@users.noreply.github.com>
2021-02-06 16:56:32 -0500

    Update keyword_search_dao.py

--- dao/keyword_search_dao.py
+++ dao/keyword_search_dao.py
@@ -7,9 +7,6 @@ class KeywordSearchDao():
         self.__repository = repository.Repository()
 
     def select_keyword_search(self):
-        # defino las sentencia sql
-
+        # defino la sentencia sql
         sql_select = "SELECT id_keyword, descripcion FROM public.keyword_search"
-
-
         return self.__repository.select_keyword_search(sql_select)
